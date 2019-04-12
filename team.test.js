d96de1189bd521d9ea214390a664da6c3c994e14
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-04-12 13:00:01 -0400

    created first test

--- team.test.js
+++ team.test.js
@@ -1,5 +1,11 @@
 const teamObj = require('./team.js');
 
-// test('', () => {
-//
-// });
+test('can create a new team with no params', () => {
+    newTeam = teamObj();
+    const expectedTeam = {
+        teamName: undefined,
+        level: undefined,
+        points: 0
+    }
+    expect(expectedTeam).toEqual(newTeam)
+})
